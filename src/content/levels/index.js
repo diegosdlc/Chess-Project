2aa2a93a7a9cd029fca46ec830b9262ae010cc9f
diegosdlc <diegosdlc@users.noreply.github.com>
2026-08-20 18:39:27 +0200

    Route mechanics labs through shared registry

--- src/content/levels/index.js
+++ src/content/levels/index.js
@@ -1,19 +1,23 @@
-import { createFacingLab, facingLab } from './facing-lab.js';
+import { getMechanicLabFactory, listMechanicLabs } from './labs/index.js';
 import { createTutorial01, tutorial01 } from './tutorial-01.js';
 
 const LEVEL_FACTORIES = new Map([
-  [tutorial01.id, createTutorial01],
-  [facingLab.id, createFacingLab]
+  [tutorial01.id, createTutorial01]
 ]);
 
 export function getLevel(levelId, options = {}) {
-  const createLevel = LEVEL_FACTORIES.get(levelId);
+  const createLevel = LEVEL_FACTORIES.get(levelId) ?? getMechanicLabFactory(levelId);
   return createLevel ? createLevel(options) : null;
 }
 
 export function listLevels() {
-  return [...LEVEL_FACTORIES.keys()].map(levelId => getLevel(levelId));
+  return [
+    ...LEVEL_FACTORIES.keys(),
+    ...listMechanicLabs().map(lab => lab.id)
+  ].map(levelId => getLevel(levelId));
 }
+
+export { listMechanicLabs };
 
 export function getNextLevel(level) {
   return level?.nextLevelId ? getLevel(level.nextLevelId, { playerFactionId: level.teams.player }) : null;
