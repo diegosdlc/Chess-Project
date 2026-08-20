7211ccc9871a3e6d98886747c64b8b667a558051
diegosdlc <diegosdlc@users.noreply.github.com>
2026-08-20 18:37:30 +0200

    Add shared level board configuration

---
+++ src/content/levels/shared.js
@@ -0,0 +1,17 @@
+export const STANDARD_BOARD = Object.freeze({
+  size: 8,
+  artwork: './assets/boards/Tablero (20260812071431).webp',
+  projection: Object.freeze({
+    width: 940,
+    height: 580,
+    origin: Object.freeze({ x: 466, y: 44 }),
+    xAxis: Object.freeze({ x: 53.75, y: 31.5 }),
+    yAxis: Object.freeze({ x: -53.625, y: 31.375 })
+  }),
+  artworkFrame: Object.freeze({
+    width: '163.404255%',
+    height: '171.724138%',
+    left: '-26.595745%',
+    top: '-51.724138%'
+  })
+});
